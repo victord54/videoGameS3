b233b3f85350c59bf6aad950b319438b297e5681
nstrub <nicolas.strub@outlook.fr>
2022-01-03 20:36:53 +0100

    Menu avec règle

--- src/menu.hpp
+++ src/menu.hpp
@@ -15,10 +15,17 @@ class Menu{
     sf::Texture textureQuitGrand;
     sf::Texture texturePlayTiny;
     sf::Texture textureQuitTiny;
+    sf::Texture textureRules;
+    sf::Texture textureRulesTiny;
+    sf::Texture textureRulesDisplay;
     sf::Sprite spritePlayGrand;
     sf::Sprite spriteQuitGrand;
     sf::Sprite spritePlayTiny;
     sf::Sprite spriteQuitTiny;
+    sf::Sprite spriteRules;
+    sf::Sprite spriteRulesTiny;
+    sf::Sprite spriteRulesScreen;
+    int ruleOpen = 0;
 
     int menuUpdate;
     int choix;
@@ -29,10 +36,19 @@ class Menu{
             textureQuitGrand.loadFromFile("ressources/quitGrand.png");
             texturePlayTiny.loadFromFile("ressources/playTiny.png");
             textureQuitTiny.loadFromFile("ressources/quitTiny.png");
+            textureRules.loadFromFile("ressources/rules.png");
+            textureRulesTiny.loadFromFile("ressources/ruleTiny.png");
+            textureRulesDisplay.loadFromFile("ressources/rulesScreen.png");
+
             spritePlayGrand.setPosition(120,100);
-            spriteQuitGrand.setPosition(130,270);
+            spriteQuitGrand.setPosition(130,220);
+            spriteRules.setPosition(150,350);
+
             spritePlayTiny.setPosition(230,100);
-            spriteQuitTiny.setPosition(200,300);
+            spriteQuitTiny.setPosition(200,280);
+            spriteRulesTiny.setPosition(200,430);
+
+            spriteRulesScreen.setPosition(0,0);
             choix = 0;
             menuUpdate = 1;
         }   
@@ -47,14 +63,31 @@ class Menu{
             spriteQuitTiny.setTexture(textureQuitTiny);
             spritePlayTiny.setTexture(texturePlayTiny);
             spriteQuitGrand.setTexture(textureQuitGrand);
+            spriteRules.setTexture(textureRules);
+            spriteRulesTiny.setTexture(textureRulesTiny);
+            spriteRulesScreen.setTexture(textureRulesDisplay);
+
             if(choix == 0) {
                 app.draw(spritePlayGrand);
                 app.draw(spriteQuitTiny);
+                app.draw(spriteRulesTiny);
             }
-            if(choix == 1) {
+            else if(choix == 1) {
                 app.draw(spritePlayTiny);
                 app.draw(spriteQuitGrand);
+                app.draw(spriteRulesTiny);
             }
+            else if(choix == 2) {
+                if(ruleOpen % 2 == 0){
+                    app.draw(spriteRulesScreen);
+                }
+                else{
+                    app.draw(spritePlayTiny);
+                    app.draw(spriteQuitTiny);
+                    app.draw(spriteRules);
+                }
+            }
+
             app.display();
         }
 
@@ -71,22 +104,33 @@ class Menu{
                 else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)){
                     if(choix == 0)          //Si on est sur play
                         menuUpdate = 0;
-                    if(choix == 1) {         //Si on est sur quit
+
+                    else if(choix == 1) {         //Si on est sur quit
                         app.close();
                         exit(0);
                     }
+
+                    else if(choix == 2) {        //Si on est sur rules
+                        printf("%d\n",ruleOpen);
+                        ruleOpen++;
+                    }
                 }
                 else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
-                    if(choix == 0)
+                    if(choix == 0 || choix == 1){
                         choix++;
-                    else
-                        choix--;
+                    }
+                    else{
+                        choix = 0;
+                    }
                 }
+
                 else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
-                    if(choix == 0)
-                        choix++;
-                    else
-                        choix--;
+                    if(choix == 2 || choix == 1){
+                        choix --;
+                    }
+                    else{
+                        choix = 2;
+                    }
                 }
             }
         }
